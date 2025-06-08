3f56f80d706033697d912a2e7231870bee864c66
Moisés Ismael Santos Gómez <38365446+moises3323@users.noreply.github.com>
2025-06-08 00:11:33 -0600

    # serverf.bootsrap

--- src/bootstrap/server.bootstrap.js
+++ src/bootstrap/server.bootstrap.js
@@ -7,8 +7,9 @@ const env = yenv();
 const inicialize = (app) => {
   return new Promise((resolve, reject) => {
     const server = http.createServer(app);
+    const port = process.env.PORT || env.PORT;
     server
-      .listen(env.PORT, '0.0.0.0')
+      .listen(port, '0.0.0.0')
       .on("listening", () => {
         printLog.info("Server running on port " + server.address().port)
         resolve(true);
